5bb8032a4f9f54edc99ef971fb0c271128f693f3
dimaswntech <dimaswntech@gmail.com>
2024-01-24 22:45:50 +0700

    make response

--- app.mjs
+++ app.mjs
@@ -1,7 +1,8 @@
 import http from "http";
 
-const server = http.createServer((req, res)=>{
-
+const server = http.createServer((req, res) => {
+    res.write("ToDoList App")
+    res.end()
 })
 
 server.listen(3000)
